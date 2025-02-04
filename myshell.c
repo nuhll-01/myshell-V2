5d444358c0e77691fedd09d6c12f39d1b844a9c5
ToastyyX <erick.cool321@icloud.com>
2025-02-04 17:21:58 -0600

    added feature to handle multiple files

--- myshell.c
+++ myshell.c
@@ -1,9 +1,19 @@
 #include "headers.h"
 
+char **createArray(int size) {
+    char **ptr = calloc(size, sizeof(int));
+    if (ptr == NULL) {
+        fprintf(stderr, "Error: memory allocation failed\n");
+        exit(1);
+    }
+    return ptr;
+}
+
 int main(int argc, char *argv[]) { 
     char input[30];
     char program[30];
-    char file[30];
+    char file1[15];
+    char file2[15];
     char *token;
     pid_t pid;
 
@@ -22,16 +32,43 @@ int main(int argc, char *argv[]) {
         // commands
         if (strcmp(token, CAT) == 0) {
             // todo
-            snprintf(program, sizeof(program), "./%s", token); 
-            token = strtok(NULL, " ");
-            snprintf(file, sizeof(file), "./%s", token);
-            
-            pid = fork();
-            if (pid == 0) { 
-                char *args[] = {program, file, NULL};
-                execvp(args[0], args);
+            snprintf(program, sizeof(program), "./%s", token);
+
+            // intialize array to store files
+            char **arr = createArray(CATSIZE);
+        
+            // store the files in the array
+            int index = 0;
+            while (token != NULL) {
+                token = strtok(NULL, " ");
+                arr[index] = token;
+                index++;
             }
-            waitpid(pid, NULL, 0);
+
+            // determine the number of files
+            int file_count = (arr[0] != 0) + (arr[1] != 0);
+
+            if (file_count == 0) { 
+                printf("%s\n", "insufficient number of arguments.");
+            }
+
+            if (file_count > 0) { 
+                snprintf(file1, sizeof(file1), "%s", arr[0]); 
+
+                if (file_count == 2) {
+                    snprintf(file2, sizeof(file2), "%s", arr[1]);
+                }
+
+                pid = fork();
+
+                if (pid == 0) {
+                    char *args[] = {program, file1, (file_count == 2) ? file2 : NULL, NULL};
+                    execvp(args[0], args);
+                }
+
+                waitpid(pid, NULL, 0);
+            }
+
         } else if (strcmp(token, HEAD) == 0) { 
             // todo
             snprintf(program, sizeof(program), "./%s", token);
