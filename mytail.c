85daacc91cc29e575c99a9abffb64c4cec7c4b3d
ToastyyX <erick.cool321@icloud.com>
2025-02-11 22:30:58 -0600

    simple modifications

--- mytail.c
+++ mytail.c
@@ -1,6 +1,15 @@
 #include "headers.h"
 
-int main() { 
-    puts("Hello World!");
+/**
+ *  @brief The tail-command driver function.
+ *  
+ *  @param argc total number of command-line arguments.
+ *  @param argv array containing the command-line arguments.
+ * 
+ *  @returns 0 upon successful execution, -1 if unsucessful.
+ * 
+ */
+int main(int argc, char *argv[]) { 
+    puts("We've established a connection and developing inside a new branch");
     return 0;
 }
